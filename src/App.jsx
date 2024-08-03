4e2c3306940bacd22d69cec97611ddb92a8d0882
gopikrishnanrk <gopikrishnanrk1999@gmail.com>
2024-08-03 22:20:58 +0530

    initial-commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Footer from './common/Footer';
+import Navbar from './common/Navbar';
+import HomePage from './pages/Homepage/HomePage';
+
+function App() {
+
+  return (
+    <>
+      <Navbar />
+      <HomePage />
+      <Footer />
+    </>
+  )
+}
+
+export default App
